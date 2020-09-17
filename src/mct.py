404e4fbbb65f39ee127ba65d3f5870d9853c3c19
Claudiu <yatsukoyuno@gmail.com>
2020-09-17 10:24:07 +0300

    Added method for building mc model

--- src/mct.py
+++ src/mct.py
@@ -27,8 +27,18 @@ def read_data(path, test_size_split=0.15):
     return train_df, eval_df
 
 
+def build_model():
+    model = ClassificationModel('roberta', './outputs/checkpoint-17812-epoch-1', num_labels=10, args={
+        'learning_rate': 1e-5, 'num_train_epochs': 4, 'reprocess_input_data': True, 'overwrite_output_dir': True, "train_batch_size": 22,
+        "eval_batch_size": 12, "save_steps": 1000, })
+
+    return model
+
+
 def main():
     train_df, eval_df = read_data('./data/news2/dataset/dataset_proccesed.csv')
+
+    model = build_model()
 
 
 if __name__ == '__main__':
