12f41f5ea4b4ceb7edadd742ed45312dd042569b
Claudiu <yatsukoyuno@gmail.com>
2020-09-18 17:24:55 +0300

    Formalized ranker

--- src/swarm.py
+++ src/swarm.py
@@ -1,17 +1,11 @@
 import ast
 import json
-import os
-import random
-from collections import OrderedDict
-
-import numpy as np
+from sklearn.model_selection import train_test_split
 import pandas as pd
 
-from sklearn.feature_extraction.text import TfidfVectorizer
-from sklearn.model_selection import train_test_split
-
-from reader.reader import Reader
 from classifier.classifier import Classifier
+from ranker.ranker import Ranker
+from reader.reader import Reader
 
 
 def read_data(path, test_size_split=0.15):
@@ -20,38 +14,6 @@ def read_data(path, test_size_split=0.15):
     train_df, eval_df = train_test_split(df, test_size=test_size_split)
 
     return train_df, eval_df
-
-
-def get_best_contexts(question, contexts, top_k):
-    vectorizer = TfidfVectorizer(
-        lowercase=True,
-        stop_words='english',
-        ngram_range=(1, 1),
-    )
-
-    df = pd.DataFrame.from_dict(contexts)
-
-    tfidf_matrix = vectorizer.fit_transform(df["text"])
-
-    question_vec = vectorizer.transform([question])
-    scores = tfidf_matrix.dot(question_vec.T).toarray()
-
-    idx_scores = [(idx, score) for idx, score in enumerate(scores)]
-    indices_and_scores = OrderedDict(
-        sorted(idx_scores, key=(lambda tup: tup[1]), reverse=True)
-    )
-
-    df_sliced = df.loc[indices_and_scores.keys()]
-    df_sliced = df_sliced[:top_k]
-
-    conte = list(df_sliced.text.values)
-    meta_data = [{"date": row["date"]} for _, row in df_sliced.iterrows()]
-
-    ranked_contexts = []
-    for para, meta in zip(contexts, meta_data):
-        ranked_contexts.append(para)
-
-    return ranked_contexts
 
 
 def get_labels():
@@ -65,14 +27,16 @@ def get_labels():
 def main():
 
     labels = get_labels()
-    ranker = Classifier(model_path='models/MCT', num_labels=len(labels))
+
+    classifier = Classifier(model_path='models/MCT', num_labels=len(labels))
+    ranker = Ranker()
     reader = Reader(model_path='models/QA')
 
     # Infer on any question
     print("Ready to roll")
     while True:
         question = input()
-        group = ranker.predict(question, labels)
+        group = classifier.predict(question, labels)
         print(group)
 
         contexts = []
@@ -85,7 +49,8 @@ def main():
 
             print(len(contexts))
 
-            ranked_contexts = get_best_contexts(question, contexts, 5)
+            ranked_contexts = ranker.get_best_contexts(question, contexts, 5)
+
             for context in ranked_contexts:
                 answers, probabilities = reader.predict(
                     question, context['text'])
