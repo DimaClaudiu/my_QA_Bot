c5b280c70e168775df5b4e195f1429474d678fd0
Claudiu <yatsukoyuno@gmail.com>
2020-09-21 13:07:18 +0300

    Implemented rank method using facebooks' dpr

--- src/ranker/DensePassageRanker.py
+++ src/ranker/DensePassageRanker.py
@@ -1,18 +1,38 @@
 from ranker.ranker import Ranker
 
-from transformers.modeling_dpr import DPRContextEncoder, DPRQuestionEncoder
-from transformers.tokenization_dpr import DPRContextEncoderTokenizer, DPRQuestionEncoderTokenizer
+from transformers import DPRReader, DPRReaderTokenizer
+import torch
 
 
 class DensePassageRanker(Ranker):
-    def __init__(self, question_encoder_path="facebook/dpr-question_encoder-single-nq-base", context_encoder_path="facebook/dpr-ctx_encoder-single-nq-base"):
 
-        self.question_tokenizer = DPRQuestionEncoderTokenizer.from_pretrained(
-            question_encoder_path)
-        self.question_encoder = DPRQuestionEncoder.from_pretrained(
-            question_encoder_path).to(self.device)
+    def __init__(self):
+        self.tokenizer = DPRReaderTokenizer.from_pretrained(
+            'facebook/dpr-reader-single-nq-base')
+        self.model = DPRReader.from_pretrained(
+            'facebook/dpr-reader-single-nq-base', return_dict=True)
 
-        self.context_tokenizer = DPRContextEncoderTokenizer.from_pretrained(
-            context_encoder_path)
-        self.context_encoder = DPRContextEncoder.from_pretrained(
-            context_encoder_path).to(self.device)
+    def rank(self, question, contexts, top_k):
+        encoded_inputs = self.tokenizer(
+            questions=[question],
+            titles=[""],
+            texts=contexts,
+            return_tensors='pt'
+        )
+        input_ids = encoded_inputs["input_ids"].tolist()[0]
+
+        outputs = self.model(**encoded_inputs)
+        start_logits = outputs.start_logits
+        end_logits = outputs.end_logits
+        relevance_logits = outputs.relevance_logits
+
+        answer_start = torch.argmax(
+            start_logits
+        )
+
+        answer_end = torch.argmax(end_logits) + 1
+
+        answer = tokenizer.convert_tokens_to_string(
+            tokenizer.convert_ids_to_tokens(input_ids[answer_start:answer_end]))
+
+        return answer
