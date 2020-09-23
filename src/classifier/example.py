d2679d3b4dc56cb862eb01daed3e12810060c59f
Claudiu <yatsukoyuno@gmail.com>
2020-09-23 13:16:57 +0300

    Added evaluation example

--- src/classifier/example.py
+++ src/classifier/example.py
@@ -6,7 +6,6 @@ from tensorflow.keras.utils import to_categorical
 
 def demo_train():
     data = pd.read_csv('data/news2/dataset/dataset_proccesed.csv')
-    data = data[0:1000]
 
     data = data.dropna()
 
@@ -22,6 +21,25 @@ def demo_train():
     model.train(x_train, y_train, './demoModel.h5', epochs=1)
 
 
+def demo_eval(model_path):
+    data = pd.read_csv('data/news2/dataset/dataset_proccesed.csv')
+
+    data = data.dropna()
+
+    data, data_test = train_test_split(
+        data, test_size=0.2)
+
+    model = TfClassifier()
+    model.load('demoModel.h5')
+
+    x_test = data_test['text'].to_list()
+    y_test = to_categorical(data_test['labels'])
+
+    eval_results = model.eval(x_test, y_test)
+
+    print(eval_results)
+
+
 def demo_predict():
     model = TfClassifier()
     model.load('demoModel.h5')
@@ -32,6 +50,8 @@ def demo_predict():
 
 
 if __name__ == '__main__':
+    demo_eval()
+
     demo_train()
 
     demo_predict()
