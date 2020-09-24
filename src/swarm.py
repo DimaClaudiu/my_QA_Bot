c79870937f0bdfc70fd6f88420a261fb657757fe
Claudiu <yatsukoyuno@gmail.com>
2020-09-24 22:37:13 +0300

    Added presentention methods

--- src/swarm.py
+++ src/swarm.py
@@ -1,14 +1,25 @@
 import ast
 import json
-from sklearn.model_selection import train_test_split
-import pandas as pd
+import os
 
-from classifier.StClassifier import StClassifier
+import pandas as pd
+from sklearn.model_selection import train_test_split
+
 from classifier.TfClassifier import TfClassifier
 from ranker.tfidfRanker import TfidfRanker
-from reader.StReader import StReader
 from reader.PtReader import PtReader
 from reader.TfReader import TfReader
+
+
+from silence_tensorflow import silence_tensorflow
+silence_tensorflow()
+
+
+LABEL_PATH = 'chatBot/data/news2/classes.json'
+DB_PATH = 'chatBot/data/news2/split_contexts/'
+
+CLASSIFIER_PATH = 'chatBot/models/MCT/tf_bert_base_1.h5'
+READER_PATH = 'chatBot/models/QA/electra_squad2'
 
 
 def read_data(path, test_size_split=0.15):
@@ -20,11 +31,35 @@ def read_data(path, test_size_split=0.15):
 
 
 def get_labels():
-    with open('data/news2/classes.json') as json_file:
+    with open(LABEL_PATH) as json_file:
         mappings = json.load(json_file)
         inv_map = {v: k for k, v in mappings.items()}
 
     return inv_map
+
+
+input_lock = False
+
+
+def process_input(shown):
+    global input_lock
+    if input_lock or shown == 0 or shown % 5 != 0:
+        return 0
+
+    print('s: show more, n: new question, q: quit')
+
+    inpt = input()
+
+    if inpt.lower() == 's':
+        input_lock = True
+        return 0
+    if inpt.lower() == 'n':
+        return 1
+    elif inpt.lower() == 'q':
+        exit(code=0)
+    else:
+        process_input(shown)
+        return 0
 
 
 def main():
@@ -32,21 +67,26 @@ def main():
     labels = get_labels()
 
     classifier = TfClassifier()
-    classifier.load('models/MCT/tfmct1/tfmlc.h5')
+    classifier.load(CLASSIFIER_PATH)
     ranker = TfidfRanker()
-    reader = TfReader(
-        model_path='models/QA/tf_bert_squad1')
+    reader = TfReader()
+    reader.load(
+        model_path=READER_PATH)
 
     # Infer on any question
-    print("Ready to roll")
     while True:
+        print("Ready to roll")
         question = input()
         label_id = classifier.predict(question)
         group = labels[label_id]
         print(group)
 
         contexts = []
-        with open('data/news2/split_contexts/' + group + '.txt', 'r', encoding='utf-8') as f:
+
+        shown = 0
+        predicted = 0
+        global input_lock
+        with open(DB_PATH + group + '.txt', 'r', encoding='utf-8') as f:
             lines = f.readlines()
 
             for line in lines:
@@ -55,15 +95,31 @@ def main():
 
             print(len(contexts))
 
-            ranked_contexts = ranker.rank(question, contexts, 5)
+            ranked_contexts = ranker.rank(question, contexts, 0)
 
-            for context in ranked_contexts:
-                print(context['text'])
+            for _, row in ranked_contexts.iterrows():
+                date = row['date']
+                text = row['text']
+
                 answer, probability = reader.predict(
-                    question, context['text'])
+                    question, text)
+                predicted += 1
 
                 if answer != '':
-                    print(f'^^ {answer} - {probability} ^^\n')
+                    pretty_probability = str(probability*100)[0:3] + '%'
+
+                    print(f'{date}: {answer} - {pretty_probability}')
+                    print(f'context: "{text}"\n')
+
+                    input_lock = False
+                    shown += 1
+
+                elif predicted % 50 == 0:
+                    print(predicted)
+
+                new_question = process_input(shown)
+                if new_question:
+                    break
 
 
 if __name__ == '__main__':
