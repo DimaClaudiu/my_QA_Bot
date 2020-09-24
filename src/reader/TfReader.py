c250d19ade34d23a0b6d9b0b447238f1527f6a4f
Claudiu <yatsukoyuno@gmail.com>
2020-09-24 10:26:36 +0300

    Added train method

--- src/reader/TfReader.py
+++ src/reader/TfReader.py
@@ -2,7 +2,8 @@ import math
 
 import tensorflow as tf
 from transformers import AutoTokenizer, TFAutoModelForQuestionAnswering
-
+from transformers import TFDistilBertForQuestionAnswering
+from transformers import DistilBertTokenizerFast
 from reader.reader import Reader
 
 
@@ -36,9 +37,47 @@ class TfReader(Reader):
             probabilities.append(self._compute_softmax(start_score))
 
         probability = (probabilities[0][answer_start] +
-                       probabilities[0][answer_end-1])/2
+                       probabilities[0][answer_end - 1]) / 2
 
         return answer, probability
+
+    def train(self, train_contexts, train_questions, train_answers, base_model='distilbert-base-uncased', epochs=10, batch_size=10):
+
+        tokenizer = DistilBertTokenizerFast.from_pretrained(
+            base_model)
+
+        self._add_end_idx(train_answers, train_contexts)
+
+        train_encodings = tokenizer(
+            train_contexts, train_questions, truncation=True, padding=True)
+
+        train_encodings = tokenizer(
+            train_contexts, train_questions, truncation=True, padding=True)
+
+        self._add_token_positions(train_encodings, train_answers, tokenizer)
+
+        train_dataset = tf.data.Dataset.from_tensor_slices((
+            {key: train_encodings[key]
+                for key in ['input_ids', 'attention_mask']},
+            {key: train_encodings[key]
+                for key in ['start_positions', 'end_positions']}
+        ))
+
+        self.model = TFDistilBertForQuestionAnswering.from_pretrained(
+            base_model)
+
+        # Keras will expect a tuple when dealing with labels
+        train_dataset = train_dataset.map(lambda x, y: (
+            x, (y['start_positions'], y['end_positions'])))
+
+        loss = tf.keras.losses.SparseCategoricalCrossentropy(from_logits=True)
+        self.model.distilbert.return_dict = False
+
+        # fit model
+        optimizer = tf.keras.optimizers.Adam(learning_rate=5e-5)
+        self.model.compile(optimizer=optimizer, loss=loss)
+        self.model.fit(train_dataset.shuffle(
+            1000).batch(batch_size), epochs=epochs, batch_size=batch_size)
 
     @staticmethod
     def _compute_softmax(scores):
@@ -58,3 +97,39 @@ class TfReader(Reader):
         for score in exp_scores:
             probs.append(score / total_sum)
         return probs
+
+    @staticmethod
+    def _add_token_positions(encodings, answers, tokenizer):
+        start_positions = []
+        end_positions = []
+        for i in range(len(answers)):
+            start_positions.append(encodings.char_to_token(
+                i, answers[i]['answer_start']))
+            end_positions.append(encodings.char_to_token(
+                i, answers[i]['answer_end'] - 1))
+            # if None, the answer passage has been truncated
+            if start_positions[-1] is None:
+                start_positions[-1] = tokenizer.model_max_length
+            if end_positions[-1] is None:
+                end_positions[-1] = tokenizer.model_max_length
+        encodings.update({'start_positions': start_positions,
+                          'end_positions': end_positions})
+
+    @staticmethod
+    def _add_end_idx(answers, contexts):
+        for answer, context in zip(answers, contexts):
+            gold_text = answer['text']
+            start_idx = answer['answer_start']
+            end_idx = start_idx + len(gold_text)
+
+            # sometimes squad answers are off by a character or two – fix this
+            if context[start_idx:end_idx] == gold_text:
+                answer['answer_end'] = end_idx
+            elif context[start_idx-1:end_idx-1] == gold_text:
+                answer['answer_start'] = start_idx - 1
+                # When the gold label is off by one character
+                answer['answer_end'] = end_idx - 1
+            elif context[start_idx-2:end_idx-2] == gold_text:
+                answer['answer_start'] = start_idx - 2
+                # When the gold label is off by two characters
+                answer['answer_end'] = end_idx - 2
